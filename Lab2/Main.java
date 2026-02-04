cef7524594c615c9ef81ba5de65e007ae2bbd788
oakgreystudent <fa624456@wne.edu>
2026-02-04 18:17:20 -0500

    add comments
    add comments

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -4,52 +4,58 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Person user2 = new Person();
-	
+
+		// take user's name as input
 		Scanner username = new Scanner(System.in); //create scanner object
 		System.out.println("Enter your name:");
 		String name = username.nextLine(); //read user input
 	
 		user2.setName(name);
-		System.out.println(user2.getName());
+		System.out.println(user2.getName()); // print user's name
 		
 		//username.close();
-		
+
+		// take user's income as input
 		Scanner Income= new Scanner(System.in); //create scanner object
 		System.out.println("Enter your income:");
 		double money = username.nextDouble(); //read user input
 	
 		user2.setIncome(money);
-		System.out.println(user2.getIncome());
+		System.out.println(user2.getIncome()); // print user's income
 		
 		//Income.close();
-		
+
+		// take user's SSN as input
 		Scanner SSN = new Scanner(System.in); //create scanner object
 		System.out.println("Enter your SSN:");
 		String ssn = SSN.next(); //read user input
 	
 		user2.setSSN(ssn);
-		System.out.println(user2.getSSN());
+		System.out.println(user2.getSSN()); // print SSN
 		
 		//SSN.close();
-		
+
+		// take user's credit score as input
 		Scanner CreditScoreInput= new Scanner(System.in); //create scanner object
 		System.out.println("Enter your credit score:");
 		double CreditScore = CreditScoreInput.nextDouble(); //read user input
 	
 		user2.setCreditScore(CreditScore);
-		System.out.println(user2.getCreditScore());
-		
+		System.out.println(user2.getCreditScore()); // print credit score
+
+		// ask for user's choice or home or car loan
 		Scanner Choice = new Scanner(System.in); //create scanner object
 		System.out.println("Do you want a home loan or car loan:");
 		String answer = Choice.nextLine(); //read user input
-	
+
+		// check user input to see if it meets parameter
 		Loan Userloan = new Loan(); 
 		if (answer.equalsIgnoreCase("Home loan")) {
-			Userloan.HomeLoan(user2.getIncome(), user2.getCreditScore());
+			Userloan.HomeLoan(user2.getIncome(), user2.getCreditScore()); // pass income and credit score to function
 		
 		}
 		else if (answer.equalsIgnoreCase("Car loan")) {
-			Userloan.CarLoan(user2.getIncome(), user2.getCreditScore());
+			Userloan.CarLoan(user2.getIncome(), user2.getCreditScore()); // pass income and credit score to function
 		}
 		SSN.close();
 		Income.close();
@@ -57,4 +63,5 @@ public class Main {
 		Choice.close();
 		CreditScoreInput.close();
 	}
+
 }
