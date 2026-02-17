41d0654eb7f76b2f836e72ea9c8ca88ea6adb7aa
oakgreystudent <fa624456@wne.edu>
2026-02-16 21:03:51 -0500

    Update Lab4Driver.java
    -created array and added class objects
    Still need to...
    - make sure array.sort() works [it'll only work if the compareTo function in raceable.java is working]

--- Lab4/src/lab4/Lab4Driver.java
+++ Lab4/src/lab4/Lab4Driver.java
@@ -1,18 +1,40 @@
 package lab4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lab4Driver {
 	
     public static void main(String[] args) {
 
 		// class instantiations
 		Horse horse = new Horse("Max", 5.5, 400, 2006, "white", "stallion");
+		Horse horse2 = new Horse("Mary", 5.5, 400, 2006, "white", "stallion");
 		//horse.setaddress("453 Rearheaert Dr");
 		RaceHorse racehorse = new RaceHorse("Max", 5.5, 400, 2006, "white", "stallion", "12345567", 2010, 10, 555.55);
+		RaceHorse racehorse2 = new RaceHorse("Mary", 5.5, 400, 2006, "white", "stallion", "12345567", 2010, 10, 555.55);
+				
+		/*
+		Lab4Driver[] arr;
+		arr = new Lab4Driver[4];
+		arr[0] = new Lab4Driver(horse);
+		*/
 		
-		// printing
-		System.out.println(horse.getName());
-		System.out.println("Race Horse Name: " + horse.getName() + ", Height: " + horse.getHeight() + ", Weight: " + horse.getWeight() + ", Year born: " + horse.getYearBorn() + ", Color: " + horse.getColor() + ", Breed:" + horse.getBreed());
-		System.out.println(racehorse.getName() + racehorse.getregistrationNum());
+		// create array and store class objects
+		List<Horse> array = new ArrayList<Horse>();
+		array.add(horse);
+		array.add(horse2);
+		array.add(racehorse);
+		array.add(racehorse2);
+		
+		System.out.println("*****UNSORTED ARRAY*****");
+		// print the array
+		System.out.println(array);
+		System.out.println("*****SORTED ARRAY*****");
+		// sort the array
+		//array.sort()
+		System.out.println(array);
 
     }
 }
