612ebf55b3fab56fbb1362450fd6bfee402caa29
oakgreystudent <fa624456@wne.edu>
2026-02-18 13:23:09 -0500

    Update Raceable.java
    work in progress... change to interface

--- Lab4/src/lab4/Raceable.java
+++ Lab4/src/lab4/Raceable.java
@@ -1,23 +1,7 @@
 package lab4;
-import java.util.Arrays;
 
-public class Racebale implements Comparable<Raceable> {
-
-	double averageWinnings;
-	double getWinnings;
-	
-	// getter for averageWinnings
-	public double getAverageWinnings() {
-		return averageWinnings; // output name
-	}
-	
-	@Override
-	public int compareTo(final Raceable raceable) {
-		if(raceable == null ) {
-			return -1;
-		} else {
-			return (int)(this.averageWinnings - raceable.getAverageWinnings);
-		}
-	 }
+public interface Raceable {
+	double getAverageWinnings();
+	double getWinnings();
 	
 }
