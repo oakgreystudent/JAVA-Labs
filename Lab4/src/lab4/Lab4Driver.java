776728f5949fb062ec13e19b43f825fd05040e26
oakgreystudent <fa624456@wne.edu>
2026-02-18 13:16:08 -0500

    Update Lab4Driver.java
    working in progress trying to implement the array.sort or collections.sort method

--- Lab4/src/lab4/Lab4Driver.java
+++ Lab4/src/lab4/Lab4Driver.java
@@ -1,7 +1,9 @@
 package lab4;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Lab4Driver {
@@ -9,31 +11,20 @@ public class Lab4Driver {
     public static void main(String[] args) {
 
 		// class instantiations
-		Horse horse = new Horse("Max", 5.5, 400, 2006, "white", "stallion");
-		Horse horse2 = new Horse("Mary", 5.5, 400, 2006, "white", "stallion");
-		//horse.setaddress("453 Rearheaert Dr");
 		RaceHorse racehorse = new RaceHorse("Max", 5.5, 400, 2006, "white", "stallion", "12345567", 2010, 10, 555.55);
 		RaceHorse racehorse2 = new RaceHorse("Mary", 5.5, 400, 2006, "white", "stallion", "12345567", 2010, 10, 555.55);
-				
-		/*
-		Lab4Driver[] arr;
-		arr = new Lab4Driver[4];
-		arr[0] = new Lab4Driver(horse);
-		*/
 		
 		// create array and store class objects
-		List<Horse> array = new ArrayList<Horse>();
-		array.add(horse);
-		array.add(horse2);
+		ArrayList<RaceHorse> array = new ArrayList<RaceHorse>();
 		array.add(racehorse);
 		array.add(racehorse2);
 		
 		System.out.println("*****UNSORTED ARRAY*****");
-		// print the array
-		System.out.println(array);
+		System.out.println(array); // print the array
 		System.out.println("*****SORTED ARRAY*****");
 		// sort the array
-		//array.sort()
+		//Arrays.sort(array, new racehorse.compareTo());
+		Collections.sort((array);
 		System.out.println(array);
 
     }
