d7607914f3ae14757b1d921c2f6397685b8c2eb8
oakgreystudent <fa624456@wne.edu>
2026-02-18 13:21:38 -0500

    Testing compareTo method
    Work in progress... add compareTo method

--- Lab4/src/lab4/RaceHorse.java
+++ Lab4/src/lab4/RaceHorse.java
@@ -1,13 +1,13 @@
 package lab4;
 
-public class RaceHorse extends Horse{
+public class RaceHorse extends Horse implements Comparable<Raceable>{
 	private String registrationNum;
 	private int yearfirstRaced;
 	private int numRaces;
 	private double winnings;
 	
 	// constructor
-	RaceHorse(String name, double height, double weight, int yearBorn, String color, String breed,
+	public RaceHorse(String name, double height, double weight, int yearBorn, String color, String breed,
 			String registrationNum, int yearfirstRaced, int numRaces, double winnings) {
 		super(name, height, weight, yearBorn, color, breed);
 		
@@ -39,26 +39,35 @@ public class RaceHorse extends Horse{
 	
 	// getter for registrationNum
 	public String getRegistrationNum() {
-		return registrationNum; // output name
+		return registrationNum; // output 
 	}
 	
 	// getter for yearfirstRaced
 	public int getYearfirstRaced() {
-		return yearfirstRaced; // output name
+		return yearfirstRaced; // output 
 	}
 	
 	// getter for numRaces
 	public int getNumRaces() {
-		return numRaces; // output name
+		return numRaces; // output 
 	}
 	
 	// getter for winnings
 	public double getWinnings() {
-		return winnings; // output name
+		return winnings; // output
 	}
 	
 	@Override
 	public String toString() {
 		return ("\n Race Horse Name: " + this.getName() + ", Height: " + this.getHeight() + ", Weight: " + this.getWeight() + ", Year born: " + this.getYearBorn() + ", Color: " + this.getColor() + ", Breed:" + this.getBreed() + "\n" + "======");
 	}
+	
+	// compareTo method	only works on strings
+	@Override
+	public int compareTo(Raceable r) {
+		return this.getName().compareTo(r.getName());
+	}
+	
+	
+	
 }
