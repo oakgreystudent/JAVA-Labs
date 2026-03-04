d452d1dff0f4d344ad9b3810b85659f12ac8f414
oakgreystudent <fa624456@wne.edu>
2026-03-04 18:22:56 -0500

    Update Lab5Driver.java
    semi-working state

--- Lab5/src/lab5/Lab5Driver.java
+++ Lab5/src/lab5/Lab5Driver.java
@@ -3,56 +3,52 @@ package lab5;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
+import java.util.Scanner;
+import java.lang.*;
+
 
 /**
 * The Lab5Driver class tests the PropertyFinder class
 */
 
-
 public class Lab5Driver {
 	
-	private String line;
-
-	public static void FileWriter(boolean append)
-	  throws FileNotFoundException {
-	 
-	    FileWriter fw = new FileWriter("errorlog.txt", true);
-	    BufferedWriter bw = new BufferedWriter(fw);
-	    bw.write(this.line);
-	    bw.newLine();
-	    bw.close();
-	}
-	
-	public static void main(String[] args) throws FileNotFoundException{
-	    // Create scanner to obtain input from the user
-	    Scanner input = new Scanner(System.in);
-		
-		// Prompt user for name of property file 
-	    System.out.print("Enter name of real estate file: ");
-	    String fileName = input.nextLine();
-		input.close();
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		// try-catch block
 		try {
+			
+			// Create scanner to obtain input from the user
+		    Scanner input = new Scanner(System.in);
+			
+			// Prompt user for name of property file 
+		    System.out.print("Enter name of real estate file: "); // propertieslab5.csv
+		    String fileName = input.nextLine();
+			input.close();
+			
 			// Create a PropertyFinder and initialize with entries in the filepp
 		    PropertyFinder finder = new PropertyFinder(fileName);
 		} catch (FileNotFoundException ex) {
 			System.out.println("Error:" + ex.getMessage());
 		} catch (Exception e) {
 			System.out.println("Error:" + e.getMessage());
-			
-			// try-catch block
-			try {
-				FileWriter(true);
-				appendtoFile(finder);
-			} catch (FileNotFoundException exc) {
-				System.out.println("Error:" + exc.getMessage());
-			}
-		
+				
 		}
-
-		input.close();
 		
+		Scanner scanner = new Scanner(System.in); //create scanner object
+		System.out.print("Enter latitude: ");
+		double lat1 = 67; //read user input
+		scanner.close();
+		
+		Scanner user2 = new Scanner(System.in); //create scanner object
+		System.out.println("Enter longitude:");
+		double long2 = 87; //read user input
+		user2.close();
+		
+		String closestProp = PropertyFinder.findClosest(lat1, long2);
+		System.out.println(closestProp);
 	}
 }
